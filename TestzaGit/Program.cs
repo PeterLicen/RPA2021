b418c8860eee28507ffdeb9e821653aa80ccacd8
Peter <licen.g.peter@gmail.com>
2021-01-20 17:29:33 +0100

    Nova mapa

---
+++ TestzaGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestzaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljen Git");
+            Console.ReadLine();
+        }
+    }
+}
